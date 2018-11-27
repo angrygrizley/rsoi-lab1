de040d636b83326126784efcca46e3c0e4994bb7
Catherine <katepoluektova@gmail.com>
2018-11-28 02:56:53 +0300

    Add spring boot web app

--- src/main/java/com/angrygrizley/rsoi_lab1/MainController.java
+++ src/main/java/com/angrygrizley/rsoi_lab1/MainController.java
@@ -1,4 +1,18 @@
 package com.angrygrizley.rsoi_lab1;
 
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class MainController {
+
+    @RequestMapping( value = "/reverse", method = RequestMethod.GET)
+    public String reverseStringPage(@RequestParam(value = "string", required = false, defaultValue = "привет") String string, ModelMap model) {
+        String reversed = StringWorker.getReverseString(string);
+
+        model.addAttribute("string", reversed);
+        return "reverse";
+    }
+
 }
--- src/main/java/com/angrygrizley/rsoi_lab1/StringWorker.java
+++ src/main/java/com/angrygrizley/rsoi_lab1/StringWorker.java
@@ -1,4 +1,7 @@
 package com.angrygrizley.rsoi_lab1;
 
 public class StringWorker {
+    public static String getReverseString(String string) {
+        return new StringBuilder(string).reverse().toString();
+    }
 }
