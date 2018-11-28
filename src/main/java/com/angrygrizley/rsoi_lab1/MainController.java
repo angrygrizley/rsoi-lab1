5378314fe2b2ca6f804dd55aeb78087dab4c9247
Catherine <katepoluektova@gmail.com>
2018-11-28 03:22:23 +0300

    Fix controller error

--- src/main/java/com/angrygrizley/rsoi_lab1/MainController.java
+++ src/main/java/com/angrygrizley/rsoi_lab1/MainController.java
@@ -1,10 +1,12 @@
 package com.angrygrizley.rsoi_lab1;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class MainController {
 
     @RequestMapping( value = "/reverse", method = RequestMethod.GET)
